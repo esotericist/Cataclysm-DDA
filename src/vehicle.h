96f68f4575f8efdd0cd2f1418c547cf5306d11f2
eltank <8000047+eltank@users.noreply.github.com>
2021-07-18 00:42:45 -0700

    Allow vehicle control (pldrive) to be performed by any Character.
    This unhardcodes the player character as the only Character capable of
    driving vehicles. Together with better autodrive code, it opens the door
    to potential chauffeur NPCs in the future.

--- src/vehicle.h
+++ src/vehicle.h
@@ -871,7 +871,7 @@ class vehicle
         tripoint get_autodrive_target() {
             return autodrive_local_target;
         }
-        void do_autodrive();
+        void do_autodrive( Character &driver );
         void stop_autodriving();
         /**
          *  Operate vehicle controls
@@ -1498,7 +1498,7 @@ class vehicle
          * @param p direction player is steering
          * @param z for vertical movement - e.g helicopters
          */
-        void pldrive( const point &p, int z = 0 );
+        void pldrive( Character &driver, const point &p, int z = 0 );
 
         // stub for per-vpart limit
         units::volume max_volume( int part ) const;
